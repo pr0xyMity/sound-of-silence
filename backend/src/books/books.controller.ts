87cbb7a152e9019d36ed74819278e2c216df2115
Konrad Barrek (Efix) <konrad.trade@gmail.com>
2023-06-26 21:15:30 +0200

    feat(SOS): get single book by its parameter id

--- backend/src/books/books.controller.ts
+++ backend/src/books/books.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { BookService } from './books.service';
 
 @Controller('books')
@@ -6,7 +6,12 @@ export class BooksController {
   constructor(private readonly bookService: BookService) {}
 
   @Get()
-  getBooks(): string {
+  getBooks(): string[] {
     return this.bookService.getBooks();
+  }
+
+  @Get(':id')
+  getBook(@Param('id') id: string) {
+    return this.bookService.getBook(id);
   }
 }
--- backend/src/books/books.service.ts
+++ backend/src/books/books.service.ts
@@ -2,7 +2,11 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class BookService {
-  getBooks(): string {
-    return 'Books list';
+  getBooks(): string[] {
+    return ['Book 1', 'Book 2', 'Book 3'];
+  }
+
+  getBook(id: string): string {
+    return `Book with id - ${id}`;
   }
 }
