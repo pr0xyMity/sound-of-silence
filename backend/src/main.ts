9863dd84db7ca144411c6bf2e2793553eb1753ac
Konrad Barrek (Efix) <konrad.trade@gmail.com>
2023-06-26 22:44:15 +0200

    feat(SOS): enable whitelisting for dto's to strip validation data

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(3000);
 }
 bootstrap();
