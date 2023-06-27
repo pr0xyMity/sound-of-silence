e96777c59044b04d8e0c172972ec27c88a92396c
Konrad Barrek (Efix) <konrad.trade@gmail.com>
2023-06-27 06:17:31 +0200

    feat(SOS): typeorm initial setup for local database

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BooksModule } from './books/books.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [BooksModule],
+  imports: [
+    BooksModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5433,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'postgres',
+      autoLoadEntities: true,
+      // TODO: Disable for production
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
