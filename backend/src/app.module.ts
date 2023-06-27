0c215d254f47b704bafcaf35419d5a0ad3d20dd7
Konrad Barrek (Efix) <konrad.trade@gmail.com>
2023-06-27 21:30:42 +0200

    feat(SOS): install config module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BooksModule } from './books/books.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
@@ -18,6 +19,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       // TODO: Disable for production
       synchronize: true,
     }),
+    ConfigModule.forRoot(),
   ],
   controllers: [AppController],
   providers: [AppService],
