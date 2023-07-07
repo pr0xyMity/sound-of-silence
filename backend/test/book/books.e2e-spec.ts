35d603562a3e714662c3d63317fcf15fdbf6c34b
Konrad Barrek (Efix) <konrad.trade@gmail.com>
2023-07-07 20:31:02 +0200

    feat(SOS): e2e testing uses additional database

--- backend/test/book/books.e2e-spec.ts
+++ backend/test/book/books.e2e-spec.ts
@@ -2,13 +2,26 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { BooksModule } from '../../src/books/books.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 describe('[Feature] Books - /books', () => {
   let app: INestApplication;
 
   beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [BooksModule],
+      imports: [
+        BooksModule,
+        TypeOrmModule.forRoot({
+          type: 'postgres',
+          host: 'localhost',
+          port: 5431,
+          username: 'postgres',
+          password: 'postgres',
+          database: 'postgres',
+          autoLoadEntities: true,
+          synchronize: true,
+        }),
+      ],
     }).compile();
 
     app = moduleFixture.createNestApplication();
